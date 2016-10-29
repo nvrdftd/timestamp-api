44ab95ec828995fb9026914d2c89726a60509edd
Lung Chen Huang <perfecthuang413@gmail.com>
2016-10-29 14:45:24 +0000

    finish timestamp functionality

--- server.js
+++ server.js
@@ -1,9 +1,55 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+function checkDate(req, res) {
+  var datetime = req.params.date;
+  if (isNaN(+datetime)) {
+    datetime = new Date(datetime);
+  } else {
+    datetime = new Date(+datetime * 1000);
+  }
+  
+  // translate month number to string
+  var month = new Array(12);
+  month[0] = 'January';
+  month[1] = 'February';
+  month[2] = 'March';
+  month[3] = 'April';
+  month[4] = 'May';
+  month[5] = 'June';
+  month[6] = 'July';
+  month[7] = 'August';
+  month[8] = 'September';
+  month[9] = 'October';
+  month[10] = 'November';
+  month[11] = 'December';
+  
+  var results = {
+    unix: Math.floor(datetime.getTime() / 1000),
+    natural: month[datetime.getMonth()] + " " + datetime.getDate() + ", " + datetime.getFullYear()
+  };
+  
+  
+  // handle the err
+  if (isNaN(results.unix)) {
+    res.send('Sorry! The data you typed in cannot be recognized.');
+    return
+  } else {
+    res.json(results);
+  }
+}
+
+function noInput(req, res) {
+  var results = {
+    unix: null, 
+    natural: null
+  };
+  res.json(results);
+}
+
+app.get('/:date', checkDate);
+
+app.get('/', noInput);
 
 app.listen(8080, function() {
     console.log('Listening on port 8080...')
